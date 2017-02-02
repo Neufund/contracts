847b54dd517b71f6fc40621ac71a28bb21ebb8dc
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-02 12:55:35 +0100

    Make NeufundTestToken non-abstract

--- contracts/NeufundTestToken.sol
+++ contracts/NeufundTestToken.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.4.2;
 import "./StandardToken.sol";
 
 contract NeufundTestToken is StandardToken {
+    function NeufundTestToken(uint _totalSupply) StandardToken(_totalSupply) {}
 }
