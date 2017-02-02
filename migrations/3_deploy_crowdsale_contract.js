fa12c52231dd91e2485433cbd9df2161b8839c7a
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-02 15:21:18 +0100

    Add migration for a crowdsale contract

---
+++ migrations/3_deploy_crowdsale_contract.js
@@ -0,0 +1,26 @@
+var NeufundTestToken = artifacts.require("./NeufundTestToken.sol");
+var Crowdsale = artifacts.require("./Crowdsale.sol");
+
+module.exports = function (deployer) {
+    /**
+     * address ifSuccessfulSendTo,
+     * uint fundingGoalInEthers,
+     * uint durationInMinutes,
+     * uint etherCostOfEachToken,
+     * Token addressOfTokenUsedAsReward
+     */
+    NeufundTestToken.deployed().then((instance)=> {
+        console.log(instance.address);
+        var ifSuccessfulSendTo = "0xb67fb67eb9e4700c90f2ab65c8ecbb3b687d49c7";
+        var fundingGoalInEthers = 1;
+        var durationInMinutes = 1;
+        var etherCostOfEachToken = 1;
+        var addressOfTokenUsedAsReward = instance.address;
+        deployer.deploy(Crowdsale,
+            ifSuccessfulSendTo,
+            fundingGoalInEthers,
+            durationInMinutes,
+            etherCostOfEachToken,
+            addressOfTokenUsedAsReward);
+    });
+};
