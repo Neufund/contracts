f44adaf72b6ee31b132f8971b18a68689079005b
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-02 23:07:19 +0100

    Fix tests with newly created contracts

--- test/TestNeufundTestToken.sol
+++ test/TestNeufundTestToken.sol
@@ -17,7 +17,7 @@ contract TestNeufundTestToken {
     uint initialSupplyInEther = 10000;
     NeufundTestToken ntt = new NeufundTestToken(initialSupplyInEther);
 
-    Assert.equal(ntt.balanceOf(tx.origin), initialSupplyInEther * 1 ether, "Owner should have 10000 NeufundTestToken initially");
+    Assert.equal(ntt.balanceOf(this), initialSupplyInEther * 1 ether, "Owner should have 10000 NeufundTestToken initially");
   }
 
   function testTransferTokens() {
@@ -28,7 +28,7 @@ contract TestNeufundTestToken {
     Assert.equal(ntt.balanceOf(receiver), 0, "Receiver should have an empty balance initially");
     ntt.transfer(receiver, transferValueInEther * 1 ether);
     Assert.equal(ntt.balanceOf(receiver), transferValueInEther * 1 ether, "Receiver should receive 100 tokens");
-    Assert.equal(ntt.balanceOf(tx.origin), (initialSupplyInEther - transferValueInEther) * 1 ether, "Owner should lose 100 tokens");
+    Assert.equal(ntt.balanceOf(this), (initialSupplyInEther - transferValueInEther) * 1 ether, "Owner should lose 100 tokens");
   }
 
 }
