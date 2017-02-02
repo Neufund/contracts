d5c59b9ef2ec6f666e18034291ef3b94016253df
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-02 22:57:32 +0100

    Remove default builder from truffle.js

--- truffle.js
+++ truffle.js
@@ -1,16 +1,6 @@
 var DefaultBuilder = require("truffle-default-builder");
 
 module.exports = {
-  build: new DefaultBuilder({
-    "index.html": "index.html",
-    "app.js": [
-      "javascripts/app.js"
-    ],
-    "app.css": [
-      "stylesheets/app.css"
-    ],
-    "images/": "images/"
-  }),
   networks: {
     development: {
       host: "localhost",
