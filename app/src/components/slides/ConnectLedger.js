6592a8291dbc08e454899011f95ef12534de46ce
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-08 00:56:22 +0100

    Implement Connect Ledger slide

--- app/src/components/slides/ConnectLedger.js
+++ app/src/components/slides/ConnectLedger.js
@@ -6,7 +6,24 @@ class ConnectLedger extends React.Component {
     render() {
         return (
             <Slide linkTo="/confirm_data">
-                <p>Connect your Ledger</p>
+                <div className={styles.step1}>
+                    <p className={styles.paragraph}>
+                        <h3>Please connect your Neufund wallet</h3>
+                        <span>The hardware wallet acts as an additional security layer for this account</span>
+                    </p>
+                </div>
+                <div className={styles.step2}>
+                    <p className={styles.paragraph}>
+                        <h3>Enter the PIN provided</h3>
+                        <span>This was provided to you via email</span>
+                    </p>
+                </div>
+                <div className={styles.step3}>
+                    <p className={styles.paragraph}>
+                        <h3>Navigate and open the `Ethereum` App</h3>
+                        <span>This will allow the wallet to connect to the ethereum block-chain</span>
+                    </p>
+                </div>
             </Slide>
         );
     }
