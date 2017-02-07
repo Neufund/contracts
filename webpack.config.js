b7af45c22f71831006555605055e629b4ba4fc66
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-07 13:09:10 +0100

    Remove babel config from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -25,11 +25,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015'],
-          plugins: ['transform-runtime']
-        }
+        loader: 'babel-loader'
       }
     ]
   }
