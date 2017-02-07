526995d328007c7620b023d60b91e1ed6f090124
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-07 15:01:34 +0100

    Enable css modules

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,45 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './app/javascripts/app.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'app.js'
-  },
-  plugins: [
-    // Copy our app's index.html to the build folder.
-    new CopyWebpackPlugin([
-      { from: './app/index.html', to: "index.html" }
-    ])
-  ],
-  module: {
-    rules: [
-      {
-       test: /\.css$/,
-       use: [ 'style-loader', 'css-loader' ]
-      },
-      { test: /\.json$/, use: 'json-loader' },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.(jpg|png)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 25000,
-        }
-      },
-    ]
-  }
+    entry: './app/javascripts/app.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.js'
+    },
+    plugins: [
+        // Copy our app's index.html to the build folder.
+        new CopyWebpackPlugin([
+            {from: './app/index.html', to: "index.html"}
+        ])
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            localIdentName: "[name]__[local]___[hash:base64:5]"
+                        }
+                    }
+                ]
+            },
+            {test: /\.json$/, use: 'json-loader'},
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(jpg|png)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 25000,
+                }
+            },
+        ]
+    }
 }
