81e0937630e65e786e6cb09c1fa3a5c2f66e97e4
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-07 22:08:10 +0100

    Add initial BuyEth slide implementation

--- app/src/components/slides/BuyEth.js
+++ app/src/components/slides/BuyEth.js
@@ -6,6 +6,12 @@ class BuyEth extends React.Component {
     render() {
         return (
             <Slide linkTo="/tutorial">
+                <div className={styles.wrapper}>
+                    <p>Your NeuFund wallet has been successfully setup.</p>
+                    <p>We will proceed with getting and storing some ETH which are then will be used to purchase
+                        Neumarks.</p>
+                    <div className={styles.details}></div>
+                </div>
             </Slide>
         );
     }
