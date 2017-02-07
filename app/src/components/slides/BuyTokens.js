f3fbfc1cc3df0d63e6d565b13978c90cd38c72a0
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-07 22:08:24 +0100

    Add initial BuyTokens slide implementation

--- app/src/components/slides/BuyTokens.js
+++ app/src/components/slides/BuyTokens.js
@@ -6,7 +6,11 @@ class BuyTokens extends React.Component {
     render() {
         return (
             <Slide linkTo="/confirm_transaction">
-                <p>Buy tokens</p>
+                <div className={styles.wrapper}>
+                    <p>Now that your wallet contains some ETH we will use them to purchase NMKs</p>
+                    <p>Your pre-determined ticket size is 150 NMKs</p>
+                    <div className={styles.details}></div>
+                </div>
             </Slide>
         );
     }
