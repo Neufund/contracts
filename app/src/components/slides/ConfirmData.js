8a08362974777653e8eb27bb0e1b01af0a67bad9
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-07 21:53:22 +0100

    Add initial implementation of ConfirmData slide

--- app/src/components/slides/ConfirmData.js
+++ app/src/components/slides/ConfirmData.js
@@ -6,6 +6,13 @@ class ConfirmData extends React.Component {
     render() {
         return (
             <Slide linkTo="/buy_eth">
+                <div className={styles.wrapper}>
+                    <p>Having linked your hardware wallet, we will now set up the digital counterpart of your wallet</p>
+                    <p>Please confirm your personal details</p>
+                    <div className={styles.details}>
+
+                    </div>
+                </div>
             </Slide>
         );
     }
