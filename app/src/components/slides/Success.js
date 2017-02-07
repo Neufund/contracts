384f3f0acdde1034491b45160fc8ed9a7265f556
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-07 22:08:43 +0100

    Add initial Success slide implementation

--- app/src/components/slides/Success.js
+++ app/src/components/slides/Success.js
@@ -6,7 +6,11 @@ class Success extends React.Component {
     render() {
         return (
             <Slide linkTo="/">
-                <p>You're amazing!!!</p>
+                <div className={styles.wrapper}>
+                    <h3>Transaction complete</h3>
+                    <h5>You can now continue to our ICO page and maybe increate your investment from that</h5>
+                    <div className={styles.details}></div>
+                </div>
             </Slide>
         );
     }
