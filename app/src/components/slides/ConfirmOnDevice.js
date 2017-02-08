29bb7f4f9121db3a239c66b8d7494f82e26a6302
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-08 01:15:28 +0100

    Initial implementation of ConfirmOnDevice

--- app/src/components/slides/ConfirmOnDevice.js
+++ app/src/components/slides/ConfirmOnDevice.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Slide from './Slide';
 import styles from './ConfirmOnDevice.css';
+import slideWrapper from './SlideWrapper.css'
 
 class ConfirmOnDevice extends React.Component {
     render() {
         return (
             <Slide linkTo="/success">
-                <p>Confirm on device</p>
+                <div className={slideWrapper.wrapper}>
+                    <p>Confirm on device</p>
+                    <div className={styles.confirm}></div>
+                    <div className={styles.details}></div>
+                </div>
             </Slide>
         );
     }
