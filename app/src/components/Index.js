fe94be57e06c86dfeec22ce5baa1d82fce3f1152
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-08 08:59:06 +0100

    Add shadow to Index Logo

--- app/src/components/Index.js
+++ app/src/components/Index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './Index.css';
 import {Link} from 'react-router'
+import NeufundLogo from '../../images/NeuFund_icon_light.png';
 
 class Index extends React.Component {
     render() {
         return (
             <Link to="/welcome">
-                <div className={styles.index}></div>
+                <div className={styles.index}>
+                    <img className={styles.logo} src={NeufundLogo}/>
+                </div>
             </Link>
         );
     }
