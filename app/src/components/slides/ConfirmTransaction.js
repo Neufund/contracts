ae725213dacdb58a582bb15e552d13e82e9385ef
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-08 01:15:44 +0100

    Initial implementation of ConfirmTransaction

--- app/src/components/slides/ConfirmTransaction.js
+++ app/src/components/slides/ConfirmTransaction.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Slide from './Slide';
 import styles from './ConfirmTransaction.css';
+import slideWrapper from './SlideWrapper.css'
 
 class ConfirmTransaction extends React.Component {
     render() {
         return (
             <Slide linkTo="/confirm_on_device">
-                <p>Confirm transaction</p>
+                <div className={slideWrapper.wrapper}>
+                    <p>Confirm transaction</p>
+                    <div className={styles.details}>
+
+                    </div>
+                </div>
             </Slide>
         );
     }
