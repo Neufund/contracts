3525b8715f7b02e6fd6bf1221beafe600eb1066f
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-08 09:24:38 +0100

    Fix DOM layout in ConnectLedger

--- app/src/components/slides/ConnectLedger.js
+++ app/src/components/slides/ConnectLedger.js
@@ -8,19 +8,19 @@ class ConnectLedger extends React.Component {
             <Slide linkTo="/confirm_data">
                 <div className={styles.step1}>
                     <p className={styles.paragraph}>
-                        <h3>Please connect your Neufund wallet</h3>
+                        <p>Please connect your Neufund wallet</p>
                         <span>The hardware wallet acts as an additional security layer for this account</span>
                     </p>
                 </div>
                 <div className={styles.step2}>
                     <p className={styles.paragraph}>
-                        <h3>Enter the PIN provided</h3>
+                        <p>Enter the PIN provided</p>
                         <span>This was provided to you via email</span>
                     </p>
                 </div>
                 <div className={styles.step3}>
                     <p className={styles.paragraph}>
-                        <h3>Navigate and open the `Ethereum` App</h3>
+                        <p>Navigate and open the `Ethereum` App</p>
                         <span>This will allow the wallet to connect to the ethereum block-chain</span>
                     </p>
                 </div>
