b70915ef5a44b66da617dcbdcbf13af8a24d9c8d
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-08 17:50:39 +0100

    Delete eth_app.js

--- app/src/eth_app.js
+++
@@ -1,7 +0,0 @@
-import { default as contract } from 'truffle-contract'
-
-// Import our contract artifacts and turn them into usable abstractions.
-import crowdsale_artifacts from '../../build/contracts/NeufundCrowdsaleToken.json'
-
-// CrowdsaleToken is our usable abstraction, which we'll use through the code below.
-var CrowdsaleToken = contract(crowdsale_artifacts);
