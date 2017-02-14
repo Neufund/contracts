876543c80eb560588cfbcdf06aa49ba9b3ea67dc
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-14 14:54:07 +0100

    Implement Connect Ledger slide

--- app/src/components/slides/ConnectLedger.js
+++ app/src/components/slides/ConnectLedger.js
@@ -3,11 +3,13 @@ import {hashHistory} from 'react-router';
 import Slide from './Slide';
 import styles from './ConnectLedger.css';
 import {toPromise} from '../../utils';
+import Ledger from '../../../images/nano1.png';
+import slideWrapper from './SlideWrapper.css'
 
 class ConnectLedger extends React.Component {
     constructor() {
         super();
-        this.waitForLedger();
+        // this.waitForLedger();
     }
 
     waitForLedger() {
@@ -28,23 +30,12 @@ class ConnectLedger extends React.Component {
     render() {
         return (
             <Slide>
-                <div className={styles.step1}>
-                    <div className={styles.paragraph}>
-                        <h3>Please connect your Neufund wallet</h3>
-                        <span>The hardware wallet acts as an additional security layer for this account</span>
-                    </div>
-                </div>
-                <div className={styles.step2}>
-                    <div className={styles.paragraph}>
-                        <h3>Enter the PIN provided</h3>
-                        <span>This was provided to you via email</span>
-                    </div>
-                </div>
-                <div className={styles.step3}>
-                    <div className={styles.paragraph}>
-                        <h3>Navigate and open the `Ethereum` App</h3>
-                        <span>This will allow the wallet to connect to the ethereum block-chain</span>
-                    </div>
+                <div className={slideWrapper.wrapper}>
+                    <img src={Ledger} alt="Ledger" className={styles.logo}/>
+                    <h1 className={styles.header}>Please connect your hardware wallet and open the Ethereum app to
+                        authorize your log on.</h1>
+                    <h3 className={styles.subheader}>Restricted to only pre-ICO investors</h3>
+                    <div className={styles.loader}></div>
                 </div>
             </Slide>
         );
