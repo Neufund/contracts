d111d98c5e26df10fa134562e518a8d5c31b43c2
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-14 14:33:24 +0100

    Remove language selector

--- app/src/components/slides/SlideFooter.js
+++ app/src/components/slides/SlideFooter.js
@@ -2,25 +2,15 @@ import React from 'react';
 import styles from './SlideFooter.css';
 
 class SlideFooter extends React.Component {
-  render() {
-    return (
-        <div className={styles.slideFooter}>
-            <div className={styles.languages}>
-                <select name="languages" id="languages">
-                    <option value="English" className={styles.english}>
-                        English
-                    </option>
-                    <option value="German" className={styles.german}>
-                        German
-                    </option>
-                </select>
+    render() {
+        return (
+            <div className={styles.slideFooter}>
+                <a href="/privacy">Privacy</a>
+                <a href="/faq">FAQ</a>
+                <a href="/support">Support</a>
             </div>
-            <a href="/privacy">Privacy</a>
-            <a href="/faq">FAQ</a>
-            <a href="/support">Support</a>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default SlideFooter;
