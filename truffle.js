87b6756d49a998f5d9e1dff9b56ad38367d8bd6b
Leonid Logvinov <logvinov.leon@gmail.com>
2017-03-30 16:26:04 +0200

    Add ropsten and kovan to truffle config

--- truffle.js
+++ truffle.js
@@ -9,10 +9,16 @@ module.exports = {
       port: 8545,
       network_id: '*' // Match any network id
     },
-    testnet: {
+    ropsten: {
       host: 'localhost',
       port: 8545,
       network_id: 3
+    },
+    kovan: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 42,
+      gas: 4000000
     }
   }
 }
